0da38f20d8410e7024a5aec06a5166fb3db5597c
Rogerio <rogeriosaintmartin86@gmail.com>
2023-06-21 15:28:54 -0300

    adicionando um arquivo console log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("aplicação está se conectando ao banco de dados);
+console.log("aplicação está se conectando ao banco de dados");
+console.log("O Pauloi fez um esse commit sem você ver na aula");
+console.log("Enviando para o GitHub");
